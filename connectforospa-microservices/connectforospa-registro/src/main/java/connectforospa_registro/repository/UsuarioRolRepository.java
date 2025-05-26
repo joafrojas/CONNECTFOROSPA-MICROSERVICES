d754b65a5a75fd7921c86518977c702d335cf2a5
Joan Rojas <joaf.rojas@duocuc.cl>
2025-05-26 01:04:55 -0400

    Delete connectforospa-microservices/connectforospa-registro/src/main/java/connectforospa_registro directory

--- connectforospa-microservices/connectforospa-registro/src/main/java/connectforospa_registro/repository/UsuarioRolRepository.java
+++
@@ -1,9 +0,0 @@
-package connectforospa_registro.repository;
-
-import connectforospa_registro.model.UsuarioRol;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UsuarioRolRepository extends JpaRepository<UsuarioRol, Long> {   
-}
